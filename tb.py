f606a3d6d1532b23ef535b2e63aa8265fecf517c
PyInv🐍 <pyinv@Py.local>
2025-03-11 22:23:16 +0000

    Auto-commit du : 11-03-2025 à 22:23:16

---
+++ tb.py
@@ -0,0 +1,10 @@
+tb = [19, 26, 6]
+
+if tb[0] > tb[1] and tb[0] > tb[2]:
+    print(f"c'est {tb[0]} le plus grand")
+
+elif tb[1] > tb[0] and tb[1] > tb[2]:
+        print(f"c'est {tb[1]} le plus grand")
+
+elif tb[2] > tb[0] and tb[2] > tb[1]:
+        print(f"c'est {tb[2]} le plus grand")
