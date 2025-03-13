4ee5c62d0b2f00a15d4cd92af1ba5645a7f163a7
PyInv🐍 <pyinv@Py.local>
2025-03-13 22:49:24 +0000

    Auto-commit du : 13-03-2025 à 22:49:24

--- auto_branch.py
+++ auto_branch.py
@@ -4,18 +4,30 @@ import datetime as dt
 
 Q1_Init = input("le dépot déja initialisé? Y/N :")
 
+def auto_branch(succes=""):
+    Nom_branche = input("Comment veux tu appeler ta branche? : ")
+    commit_message = input("Entre un méssage pour le commit : ")
+        
+    if commit_message=="":
+        commit_message = f"commit du : {dt.datetime.now().strftime('%d-%m-%Y à %H:%M:%S')}"
+        
+    sbp.run(["git", "checkout", "-b", Nom_branche], check=True)
+    sbp.run(["git", "add", "."], check=True)
+    sbp.run(["git", "commit", "-m", commit_message], check=True)
+    sbp.run(["git", "push", "-u", "origin", Nom_branche], check=True)
+    
+    succes="Succès"
+    
+    return succes
+    
+
+
+
+
+
 try:
     if Q1_Init == "Y" or Q1_Init=="y":
-        Nom_branche = input("Comment veux tu appeler ta branche? : ")
-        commit_message = input("Entre un méssage pour le commit : ")
-        
-        if commit_message=="":
-            commit_message = f"commit du : {dt.datetime.now().strftime('%d-%m-%Y à %H:%M:%S')}"
-        
-        sbp.run(["git", "checkout", "-b", Nom_branche], check=True)
-        sbp.run(["git", "add", "."], check=True)
-        sbp.run(["git", "commit", "-m", commit_message], check=True)
-
+        auto_branch()
 
     elif Q1_Init == "N" or Q1_Init=="n":
         link_repos = input("Entre le lien du repositorie : ")
@@ -26,17 +38,8 @@ try:
         
         sbp.run(["git", "init"], check=True)
         sbp.run(["git", "remote", "add", "origin", ""], check=True)
-        
-        Nom_branche = input("Comment veux tu appeler ta branche? : ")
-        commit_message = input("Entre un méssage pour le commit : ")
-        
-        if commit_message=="":
-            commit_message = f"commit du : {dt.datetime.now().strftime('%d-%m-%Y à %H:%M:%S')}"
-        
-        sbp.run(["git", "checkout", "-b", Nom_branche], check=True)
-        sbp.run(["git", "add", "."], check=True)
-        sbp.run(["git", "commit", "-m", commit_message], check=True)
-        sbp.run(["git", "push", "-u", "origin", Nom_branche], check=True)
+
+        auto_branch()
 
 
 except sbp.CalledProcessError as e:
