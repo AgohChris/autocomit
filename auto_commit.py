d56c4da2dd69abe8cc2484d1de8d9a5051887ad2
AgohChris <agihchris90@gmail.com>
2025-05-18 16:48:28 +0000

    Auto-commit du : 18-05-2025 à 16:48:28

--- auto_commit.py
+++ auto_commit.py
@@ -1,32 +1,31 @@
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 
 import subprocess
-import datetime as dt
+import datetime
 
-# rev
 
 def autoCommit(succes=""):
     commit_message = input("Entre un méssage pour le commit : ")
 
-    if commit_message=="":
-        Date = dt.datetime.now().strftime('%d-%m-%Y à %H:%M:%S')
-        commit_message = f"Auto-commit du : {Date}"
+    if commit_message == "":
+        commit_message = f"Auto-commit du : {datetime.datetime.now().strftime('%d-%m-%Y à %H:%M:%S')}"
 
     try:
         subprocess.run(["git", "add", "."], check=True)
         subprocess.run(["git", "commit", "-m", commit_message], check=True)
-        
-        branch = subprocess.run(["git", "rev-parse", "--abbrev-ref", "HEAD"], capture_output=True, text=True).stdout.strip()
-        
+
+        branch = subprocess.run(["git", "rev-parse", "--abbrev-ref", "HEAD"], capture_output=True,
+                                text=True).stdout.strip()
+
         subprocess.run(["git", "push", "origin", branch], check=True)
 
         succes = f"✅ Commit et push effectués sur la branche '{branch}' message : {commit_message}"
         print(succes)
 
     except subprocess.CalledProcessError as e:
-       succes = f"❌ Erreur lors de l'exécution de Git : {e}"
-       print(succes)
-        
+        succes = f"❌ Erreur lors de l'exécution de Git : {e}"
+        print(succes)
+
     return succes
 
 
